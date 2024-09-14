338073fa88e86dd7233122d58013a3b229f0e095
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-09-14 12:46:01 +0200

    add the Service annotation to the RoleService

--- src/main/java/com/personal/petcare_backend/services/role/RoleService.java
+++ src/main/java/com/personal/petcare_backend/services/role/RoleService.java
@@ -1,11 +1,13 @@
 package com.personal.petcare_backend.services.role;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.personal.petcare_backend.exceptions.role.RoleNotFoundException;
 import com.personal.petcare_backend.models.user.Role;
 import com.personal.petcare_backend.repositories.roles.RoleRepository;
 
+@Service
 public class RoleService {
 
     RoleRepository repository;
