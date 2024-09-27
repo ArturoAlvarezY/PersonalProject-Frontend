5ad79a6235c98e6c687bc7af6a0f5cdfb331d3a5
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-09-27 18:25:16 +0200

    add primevue library

--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './assets/tailwind.css';
 import 'flowbite';
-
-
-
 import App from './App.vue'
 import router from './router'
+import Primevue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Primevue, {
+    theme: {
+        preset: Aura,
+    },
+})
 
 app.mount('#app')
