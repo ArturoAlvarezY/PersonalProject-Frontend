c5dcf8cb204dd9dcb5b46a02c2c1503083ba3900
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-09-29 12:37:02 +0200

    add bootstrap icons

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./assets/tailwind.css";
+import "flowbite";
+import App from "./App.vue";
+import router from "./router";
+import Primevue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './assets/tailwind.css';
-import 'flowbite';
-import App from './App.vue'
-import router from './router'
-import Primevue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import {
+  BIconBatteryFull,
+  BIconArrow90degDown,
+  BIconBookmark,
+} from "bootstrap-icons-vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 app.use(Primevue, {
-    theme: {
-        preset: Aura,
-    },
-})
+  theme: {
+    preset: Aura,
+  },
+});
+app.use(BootstrapIconsPlugin);
+app.component("BIconBatteryFull", BIconBatteryFull);
+app.component("BIconArrow90degDown", BIconArrow90degDown);
+app.component("BIconBookmark", BIconBookmark);
 
-app.mount('#app')
+app.mount("#app");
