c65c786582db6781aa1771570b269d6c3a60c512
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-10-02 22:57:05 +0200

    making responsive the login

--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import LogoLogin from '@/components/login/LogoLogin.vue';
 import LoginComponent from '@/components/login/Title.vue';
 import FormLogIn from '@/components/login/FormLogIn.vue';
 </script>
@@ -18,5 +19,40 @@ import FormLogIn from '@/components/login/FormLogIn.vue';
 </template>
 
 <style scoped>
+.parent {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  gap: 8px;
+}
 
+.image-title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+}
+
+.login {
+  grid-column: span 2 / span 2;
+  grid-row: span 5 / span 5;
+  grid-column-start: 4;
+  grid-row-start: 1;
+}
+
+@media (max-width: 768px) {
+  .parent {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    gap: 20px;
+  }
+
+  .login {
+    grid-column: span 1 / span 1;
+    grid-row: span 1 / span 1;
+    grid-column-start: auto;
+    grid-row-start: auto;
+    margin-left: 60px;
+  }
+}
 </style>
