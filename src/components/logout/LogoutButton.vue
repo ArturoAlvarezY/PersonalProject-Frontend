4278b601ae4f7659aee27c364cf3d90c67cfadf9
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-10-03 15:07:50 +0200

    add Logout button

---
+++ src/components/logout/LogoutButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue';
+import { IoLogOut } from 'oh-vue-icons/icons';
+import logout from "@/components/logout/logout.js"
+</script>
+
+<template> 
+    <IoLogOut> </IoLogOut>
+
+</template>
