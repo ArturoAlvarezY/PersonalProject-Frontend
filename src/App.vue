11de5006fb1e59e6aa77d0ea9ca9c114583b2a40
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-10-08 08:41:48 +0200

    refacting navbars

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,7 @@ main {
   width: 100%;
   margin: 0 auto;
   background-color: white;
-  /* background-image: url("../src/assets/bumby.png"); */
   background-size: 100%;
   object-fit: cover;
 }
-
-/* @media (max-width: 480px) {
-  main {
-    background-image: url("../src/assets/bumbyresponsive.png");
-   }
-} */
 </style>
